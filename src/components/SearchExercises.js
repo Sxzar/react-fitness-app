ec87c33deee8e3b6cf622cf3fb3c261776148fe1
Sxzar <contact.sxzar@gmail.com>
2023-05-08 18:31:04 +0300

    Implemented basic ExerciseDB fetch for exercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,7 +2,19 @@ import React, { useEffect, useState } from "react";
 
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
 const SearchExercises = () => {
+    const [search, setSearch] = useState("");
+    const handleSearch = async () => {
+        if (search) {
+            const exercisesData = await fetchData(
+                "https://exercisedb.p.rapidapi.com/exercises",
+                exerciseOptions
+            );
+            console.log(exercisesData);
+        }
+    };
     return (
         <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
             <Typography
@@ -28,8 +40,8 @@ const SearchExercises = () => {
                         borderRadius: "40px",
                     }}
                     height="76px"
-                    value=""
-                    onChange={(e) => {}}
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value.toLowerCase())}
                     placeholder="Search Exercises"
                     type="text"
                 />
@@ -51,6 +63,7 @@ const SearchExercises = () => {
                         position: "absolute",
                         right: "0",
                     }}
+                    onClick={handleSearch}
                 >
                     Search
                 </Button>
