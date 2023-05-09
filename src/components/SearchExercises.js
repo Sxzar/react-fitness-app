29aa47f5505d875ad84cc607d4f9a46e33ac42cc
Sxzar <contact.sxzar@gmail.com>
2023-05-09 20:28:09 +0300

    Finalized horizontal scroll for body parts

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,9 +6,8 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     const [search, setSearch] = useState("");
-    const [exercises, setExercises] = useState([]);
     const [bodyParts, setBodyParts] = useState([]);
 
     useEffect(() => {
@@ -94,7 +93,11 @@ const SearchExercises = () => {
                 </Button>
             </Box>
             <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-                <HorizontalScrollbar data={bodyParts} />
+                <HorizontalScrollbar
+                    data={bodyParts}
+                    bodyPart={bodyPart}
+                    setBodyPart={setBodyPart}
+                />
             </Box>
         </Stack>
     );
